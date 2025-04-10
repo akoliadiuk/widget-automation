8246715b0f1cd155a905cb196564d408b73ebd83
Andrii Koliadiuk <45286839+akoliadiuk@users.noreply.github.com>
2025-04-10 22:04:48 +0300

    Added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([
+    globalIgnores(["**/node_modules/", "test-results/", "playwright-report/"]),
+    {
+        extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            parser: tsParser,
+            ecmaVersion: "latest",
+            sourceType: "module",
+
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+        },
+
+        rules: {
+            "eol-last": ["error", "always"],
+
+            "max-len": ["error", {
+                code: 100,
+            }],
+
+            "object-curly-spacing": ["error", "always"],
+        },
+    },
+]);

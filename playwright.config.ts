f028ca13cd844bba516a104f1f32ce8da7e7aa23
Andrii Koliadiuk <45286839+akoliadiuk@users.noreply.github.com>
2025-04-10 18:57:09 +0300

    Added configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['spec']],
+  reporter: [['html'], ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
